6957f5e3ae360f6c5e9f38c24443503787cfb8c8
DavidCava185 <davidveintiocho@gmail.com>
2024-05-18 18:22:25 +0200

    appView & HomeTrainer

--- src/views/AppView.vue
+++ src/views/AppView.vue
@@ -1,14 +1,6 @@
 <template>
-  <v-layout class="rounded rounded-md">
-    <v-app-bar title="Application bar"></v-app-bar>
-
-    <v-navigation-drawer>
-      <v-list>
-        <v-list-item title="Navigation drawer"></v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-
-    <v-main class="d-flex align-center justify-center" style="min-height: 300px;">
+  <v-layout class="w-100">
+    <v-main class="d-flex align-center justify-center">
       <router-view></router-view>
     </v-main>
   </v-layout>
