d89e790b8b62cb9854fa93721e77b501b98e868c
DavidCava185 <davidveintiocho@gmail.com>
2024-05-21 00:11:40 +0200

    tables home trainer

--- src/components/HomeTrainer.vue
+++ src/components/HomeTrainer.vue
@@ -1,37 +1,73 @@
 <template>
-    <div>
-        <v-row>
-            <v-col v-for="col in cols" cols="auto" md="40">
-                <v-card>
-                    <v-card-title>
-                        {{ col.title }}
-                    </v-card-title>
-                    <v-img width="40em" :src="col.img"></v-img>
-                </v-card>
-            </v-col>
-        </v-row>
+    <div class="pt-10 d-flex flex-column ga-10 align-end">
+        <v-btn width="20em" >Crear actividad</v-btn>
+        <v-card class="w-100">
+            <v-card-title>
+                Mis próximas actividades
+            </v-card-title>
+            <v-data-table :headers="headers" :items="nextActivities"></v-data-table>
+        </v-card>
+        <v-card class="w-100">
+            <v-card-title>
+                Historial de actividades
+            </v-card-title>
+            <v-data-table :headers="headers" :items="activitiesHistory"></v-data-table>
+        </v-card>
     </div>
 </template>
 
 <script lang="ts">
+import { useActivitiesStore } from '@/stores/activities/activities.store';
+import { mapActions, mapState } from 'pinia';
 import { defineComponent } from 'vue';
 
 export default defineComponent({
     name: 'HomeTrainer',
-    data () {
+    data() {
         return {
-            cols: [
+            headers: [
                 {
-                    title: 'Titulo 1',
-                    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuHLzXUcTa1vjOG9LAvOoD35s9_rbJCZf_V8gK-EwUmA&s'
+                    title: "Tipo",
+                    value: "activityType.name",
                 },
                 {
-                    title: 'Titulo 2'
+                    title: "Nombre",
+                    value: "name",
+                },
+                {
+                    title: "Duración estimada",
+                    value: "estimatedDuration",
+                },
+                {
+                    title: "Fecha y hora de inicio",
+                    value: "startDatetime",
+                },
+                {
+                    title: "Fecha y hora de finalización",
+                    value: "finishDatetime",
+                },
+                {
+                    title: "Sala",
+                    value: "room.name",
                 }
             ],
+            now: new Date(),
         }
     },
+    computed: {
+        ...mapState(useActivitiesStore, ['activities']),
+        nextActivities () {
+            return this.activities.filter((activity: any) => new Date(activity.startDatetime) > this.now);
+        },
+        activitiesHistory () {
+            return this.activities.filter((activity: any) => new Date(activity.startDatetime) <= this.now);
+        }
+    },
+    async created() {
+        await this.getAllActivities()
+    },
     methods: {
+        ...mapActions(useActivitiesStore, ['getAllActivities']),
 
     }
 });
