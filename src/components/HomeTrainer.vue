4956732d94f1112b5fb228a2d2f0cf8a73297904
DavidCava185 <davidveintiocho@gmail.com>
2024-05-21 20:14:49 +0200

    vbtn logout & vdialog

--- src/components/HomeTrainer.vue
+++ src/components/HomeTrainer.vue
@@ -1,14 +1,39 @@
 <template>
     <div class="pt-10 d-flex flex-column ga-10 align-end">
-        <v-btn width="20em" @click="">Crear actividad</v-btn>
-        
-        <v-card class="w-100">
+        <v-dialog max-width="600">
+            <template v-slot:activator="{ props: activatorProps }">
+                <v-btn rounded="lg" density="default" v-bind="activatorProps" color="red" text="Crear Actividad"
+                    variant="flat"></v-btn>
+            </template>
+
+            <template v-slot:default="{ isActive }">
+                <v-card title="Crear Actividad" color="red">
+                    <v-card-text>
+                        <v-text-field label="Nombre"></v-text-field>
+                        <v-text-field label="Duración"></v-text-field>
+                        <v-text-field label="Fecha/hora inicio"></v-text-field>
+                        <v-text-field label="Fecha/hora fin"></v-text-field>
+                        <v-text-field label="Descripción"></v-text-field>
+                        <v-text-field label="Tipo de actividad"></v-text-field>
+                        <v-text-field label="Sala"></v-text-field>
+                    </v-card-text>
+
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn text="Crear" @click="" type="submit"></v-btn>
+                        <!-- <v-btn text="cerrar" @click="isActive.value = false"></v-btn> -->
+                    </v-card-actions>
+                </v-card>
+            </template>
+        </v-dialog>
+
+        <v-card class="w-100" color="red">
             <v-card-title>
                 Mis próximas actividades
             </v-card-title>
             <v-data-table :headers="headers" :items="nextActivities"></v-data-table>
         </v-card>
-        <v-card class="w-100">
+        <v-card class="w-100" color="red">
             <v-card-title>
                 Historial de actividades
             </v-card-title>
@@ -57,10 +82,10 @@ export default defineComponent({
     },
     computed: {
         ...mapState(useActivitiesStore, ['activities']),
-        nextActivities () {
+        nextActivities() {
             return this.activities.filter((activity: any) => new Date(activity.startDatetime) > this.now);
         },
-        activitiesHistory () {
+        activitiesHistory() {
             return this.activities.filter((activity: any) => new Date(activity.startDatetime) <= this.now);
         }
     },
@@ -68,8 +93,8 @@ export default defineComponent({
         await this.getAllActivities()
     },
     methods: {
-        ...mapActions(useActivitiesStore, ['getAllActivities','createActivity']),
-        submitActivity () {
+        ...mapActions(useActivitiesStore, ['getAllActivities', 'createActivity']),
+        submitActivity() {
 
         }
     }
--- src/views/AppView.vue
+++ src/views/AppView.vue
@@ -2,7 +2,9 @@
   <v-layout class="w-100">
     <v-app-bar color="red">
       <v-img src="https://images.rawpixel.com/image_png_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIzLTA4L3Jhd3BpeGVsb2ZmaWNlOF92ZWN0b3JfbGluZV9hcnRfb2ZfZml0bmVzc19fbWluaW1hbF9maXRuZXNfbGluZV84YTlkMTJmMC0wY2I0LTRhYWItYTU3My1jNDJmMTI5NjM5ZmFfMS5wbmc.png"></v-img>
-      <v-img src="/src/assets/logout-icon.png" ></v-img>
+      <v-btn @click="logout">
+        <svg-icon type="mdi" :path="path" size="3em"></svg-icon>
+      </v-btn>
     </v-app-bar>
 
     <v-main class="d-flex align-center justify-center">
@@ -12,9 +14,24 @@
 </template>
 
 <script lang="ts">
+import { useSessionStore } from '@/stores/session/session.store';
   import { defineComponent } from 'vue';
+  import SvgIcon from '@jamescoyle/vue-icon';
+  import { mdiLogout } from '@mdi/js';
+import { mapActions } from 'pinia';
   
   export default defineComponent({
     name: 'AppView',
+    components: {
+    SvgIcon
+  },
+  methods: {
+        ...mapActions(useSessionStore, ['logout']),
+    },
+  data() {
+    return {
+       path: mdiLogout,
+    }
+  }
   })
 </script>
